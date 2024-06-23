1a9cfe6bdfed06582fafc1f93a4de69f662ee836
Arif Hassan <aarifhsn@gmail.com>
2024-06-23 23:05:46 +0600

    update root

--- assignment2/cli-app/classes/Expense.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Transaction.php";
-class Expense extends Transaction
-{
-    public function __construct($amount, $category)
-    {
-        parent::__construct($amount, $category);
-    }
-}
--- assignment2/cli-app/classes/Income.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "Transaction.php";
-class Income extends Transaction
-{
-    public function __construct($amount, $category)
-    {
-        parent::__construct($amount, $category);
-    }
-}
